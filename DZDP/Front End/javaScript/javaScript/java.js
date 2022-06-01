c50179a602c4b2eb1ddebc05dc98c7d634afaae5
AzamAkbarov <99525878+AzamAkbarov@users.noreply.github.com>
2022-06-01 15:17:37 +0500

    initial commit

---
+++ DZDP/Front End/javaScript/javaScript/java.js
@@ -0,0 +1,26 @@
+function myFunction(){
+    document.getElementById("name").innerHTML="Technology is";
+}
+
+function myFunction_2(){
+    document.getElementById("kalk").innerHTML=15+5;
+}
+
+function myFunction_3(){
+    document.getElementById("kalk_3").innerHTML=Math.sqrt(435);
+}
+
+
+window.alert('Not nothing');
+
+{
+let x,y;
+x = 8+19;
+y = x*4
+
+document.getElementById("nuts").innerHTML = y;}
+
+
+
+let text = "Nima chiqadi";
+document.getElementById("str").innerHTML = text.length;
